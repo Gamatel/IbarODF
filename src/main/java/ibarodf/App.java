6189194ed1ab765409f3b1212807e627eeebef64
Alexandre Rangom <alexandre.rangom@gmail.com>
2022-11-16 17:45:36 +0100

    <build>: Changement de la version java minimum dans le pom.xml

--- src/main/java/ibarodf/App.java
+++ src/main/java/ibarodf/App.java
@@ -1,13 +1,14 @@
 package ibarodf;
-
+import java.net.URI;
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+	public static void main( String[] args )
+	{
+		System.out.println("Hello Word!");
+	}
 }
+
