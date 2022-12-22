c12f013a71832f2a4af182cc892dc266ddb76eec
BOUZIDIIB <bouzidi.ib.ib@gamil.com>
2022-12-22 15:01:21 +0100

    refactor(ibarodf.gui.table.PropsName) Changement des valeurs

--- src/main/java/ibarodf/gui/table/PropsName.java
+++ src/main/java/ibarodf/gui/table/PropsName.java
@@ -1,30 +1,42 @@
 package ibarodf.gui.table;
 
-public class PropsName {
-    public static final String PROPS_MIMETYPE = "Mimetype";
-    public static final String PROPS_TITLE =  "Title";
-    public static final String PROPS_INITIALCREATOR =  "InitialCreator";
-    public static final String PROPS_CREATOR =  "Creator";
-    public static final String PROPS_SUBJECT =  "Subject";
-    public static final String PROPS_COMMENT =  "Comment";
-    public static final String PROPS_KEYWORDS =  "Keywords";
-    public static final String PROPS_HYPERLINKS =  "Hyperlinks";
-    public static final String PROPS_CREATIONDATE =  "CreationDate";
+import ibarodf.core.file.AbstractGenericFile;
+import ibarodf.core.file.OdfFile;
+import ibarodf.core.metadata.MetadataComment;
+import ibarodf.core.metadata.MetadataCreationDate;
+import ibarodf.core.metadata.MetadataCreator;
+import ibarodf.core.metadata.MetadataHyperlink;
+import ibarodf.core.metadata.MetadataInitialCreator;
+import ibarodf.core.metadata.MetadataKeyword;
+import ibarodf.core.metadata.MetadataStats;
+import ibarodf.core.metadata.MetadataSubject;
+import ibarodf.core.metadata.MetadataTitle;
 
-    public static final String PROPS_STATS = "Statistic";
-    public static final String PROPS_STATS_CELL_COUNT =  "cellCount";
-    public static final String PROPS_STATS_CHARACTER_COUNT =  "characterCount";
-    public static final String PROPS_STATS_DRAWN_COUNT =  "drawnCount";
-    public static final String PROPS_STATS_FRAME_COUNT =  "frameCount";
-    public static final String PROPS_STATS_IMAGE_COUNT =  "imageCount";
-    public static final String PROPS_STATS_NONWHITESPACECHARACTER_COUNT =  "nonWhitespaceCharacterCount";
-    public static final String PROPS_STATS_OBJECT_COUNT =  "objectCount";
-    public static final String PROPS_STATS_OLEOBJECT_COUNT =  "oleObjectCount";
-    public static final String PROPS_STATS_PAGE_COUNT =  "pageCount";
-    public static final String PROPS_STATS_PARAGRAPH_COUNT =  "paragraphCount";
-    public static final String PROPS_STATS_ROW_COUNT =  "rowCount";
-    public static final String PROPS_STATS_SENTENCE_COUNT =  "sentenceCount";
-    public static final String PROPS_STATS_SYLLABLE_COUNT =  "syllableCount";
-    public static final String PROPS_STATS_TABLE_COUNT =  "tableCount";
-    public static final String PROPS_STATS_WORD_COUNT =  "wordCount";
+public class PropsName {
+    public static final String PROPS_MIMETYPE = AbstractGenericFile.MIME_TYPE;
+    public static final String PROPS_TITLE = MetadataTitle.TITLE;
+    public static final String PROPS_INITIALCREATOR = MetadataInitialCreator.INITIAL_CREATOR;
+    public static final String PROPS_CREATOR = MetadataCreator.CREATOR;
+    public static final String PROPS_SUBJECT = MetadataSubject.SUBJECT;
+    public static final String PROPS_COMMENT = MetadataComment.COMMENTS;
+    public static final String PROPS_KEYWORDS = MetadataKeyword.KEYWORDS;
+    public static final String PROPS_HYPERLINKS = MetadataHyperlink.HYPERLINKS;
+    public static final String PROPS_CREATIONDATE = MetadataCreationDate.CREATION_DATE;
+
+    public static final String PROPS_STATS = MetadataStats.STATISTICS;
+    public static final String PROPS_STATS_CELL_COUNT = MetadataStats.CELL_COUNT;
+    public static final String PROPS_STATS_CHARACTER_COUNT = MetadataStats.CHARACTER_COUNT;
+    public static final String PROPS_STATS_DRAWN_COUNT = MetadataStats.DRAWN_COUNT;
+    public static final String PROPS_STATS_FRAME_COUNT = MetadataStats.FRAME_COUNT;
+    public static final String PROPS_STATS_IMAGE_COUNT = MetadataStats.IMAGE_COUNT;
+    public static final String PROPS_STATS_NONWHITESPACECHARACTER_COUNT = MetadataStats.NON_WHITE_SPACE_CHARACTER_COUNT;
+    public static final String PROPS_STATS_OBJECT_COUNT = MetadataStats.OBJECT_COUNT;
+    public static final String PROPS_STATS_OLEOBJECT_COUNT = MetadataStats.OLE_OBJECT_COUNT;
+    public static final String PROPS_STATS_PAGE_COUNT = MetadataStats.PAGE_COUNT;
+    public static final String PROPS_STATS_PARAGRAPH_COUNT = MetadataStats.PARAGRAPH_COUNT;
+    public static final String PROPS_STATS_ROW_COUNT = MetadataStats.ROW_COUNT;
+    public static final String PROPS_STATS_SENTENCE_COUNT = MetadataStats.SENTENCE_COUNT;
+    public static final String PROPS_STATS_SYLLABLE_COUNT = MetadataStats.SYLLABLE_COUNT;
+    public static final String PROPS_STATS_TABLE_COUNT = MetadataStats.TABLE_COUNT;
+    public static final String PROPS_STATS_WORD_COUNT = MetadataStats.WORD_COUNT;
 }
